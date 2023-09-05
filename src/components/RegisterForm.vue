5f3e6f22496e09868afa26b5b81bed2f3fe15f24
Dennis Ammen <dennis.ammen@outlook.com>
2023-09-05 08:19:27 +0200

    Adding translations

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -143,7 +143,9 @@ export default {
         type="checkbox"
         class="w-4 h-4 float-left -ml-6 mt-1 rounded"
       />
-      <label class="inline-block">Accept terms of service</label>
+      <i18n-t class="inline-block" keypath="register.accept" scope="global" tag="label">
+        <a href="#">{{ $t('register.tos') }}</a>
+      </i18n-t>
       <ErrorMessage class="text-red-600 block" name="tos" />
     </div>
     <button
