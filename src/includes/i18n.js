e5ec74cf278236d2aaf0027c91edf823afbb191a
Dennis Ammen <dennis.ammen@outlook.com>
2023-09-05 08:30:56 +0200

    Added german translations and number formats

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,26 @@
-import { createI18n } from 'vue-i18n';
+import { createI18n } from 'vue-i18n/dist/vue-i18n.cjs';
+import de from '@/locales/de.json';
 import en from '@/locales/en.json';
 
 export default createI18n({
-  locale: 'en',
+  locale: 'de',
   fallbackLocale: 'en',
   messages: {
+    de,
     en
+  },
+  numberFormats: {
+    de: {
+      currency: {
+        style: 'currency',
+        currency: 'EUR'
+      }
+    },
+    en: {
+      currency: {
+        style: 'currency',
+        currency: 'USD'
+      }
+    }
   }
 });
